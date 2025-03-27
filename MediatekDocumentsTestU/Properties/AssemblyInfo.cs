3b24b6eef0a4bf4046fe678747c20be3384c9ca6
cnedienne <cbouzid49@gmail.com>
2025-03-27 23:08:38 +0100

    test unitaire

---
+++ MediatekDocumentsTestU/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MediatekDocumentsTestU")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediatekDocumentsTestU")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b395e3a-0d09-4ebe-b27d-88310224aac8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MediatekDocumentsTestU/TestAbonnement.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MediatekDocumentsTests
+{
+    [TestClass]
+    public class AbonnementTest
+    {
+        private bool ParutionDansAbonnement(DateTime dateCommande, DateTime dateFinAbonnement, DateTime dateParution)
+        {
+            return dateParution >= dateCommande && dateParution <= dateFinAbonnement;
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateDansIntervalle_RetourneVrai()
+        {
+            // Arrange
+            DateTime dateCommande = new DateTime(2024, 1, 1);
+            DateTime dateFinAbonnement = new DateTime(2024, 12, 31);
+            DateTime dateParution = new DateTime(2024, 6, 15);
+
+            // Act
+            bool resultat = ParutionDansAbonnement(dateCommande, dateFinAbonnement, dateParution);
+
+            // Assert
+            Assert.IsTrue(resultat);
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateAvantCommande_RetourneFaux()
+        {
+            // Arrange
+            DateTime dateCommande = new DateTime(2024, 1, 1);
+            DateTime dateFinAbonnement = new DateTime(2024, 12, 31);
+            DateTime dateParution = new DateTime(2023, 12, 31);
+
+            // Act
+            bool resultat = ParutionDansAbonnement(dateCommande, dateFinAbonnement, dateParution);
+
+            // Assert
+            Assert.IsFalse(resultat);
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateApresFinAbonnement_RetourneFaux()
+        {
+            // Arrange
+            DateTime dateCommande = new DateTime(2024, 1, 1);
+            DateTime dateFinAbonnement = new DateTime(2024, 12, 31);
+            DateTime dateParution = new DateTime(2025, 1, 1);
+
+            // Act
+            bool resultat = ParutionDansAbonnement(dateCommande, dateFinAbonnement, dateParution);
+
+            // Assert
+            Assert.IsFalse(resultat);
+        }
+    }
+}
